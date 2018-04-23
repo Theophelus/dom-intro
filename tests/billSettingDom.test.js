92d591e6d05e49c6494e5d2ad893d5cb2c515b43
Theophelus <aneletom10@gmail.com>
2018-04-23 11:53:47 +0200

    updating bill setting tests

--- tests/billSettingDom.test.js
+++ tests/billSettingDom.test.js
@@ -2,12 +2,12 @@ describe('Bill With Settings Widget Tests', function(){
   it('should return call cost', function(){
     var newSettingBill = SettingBill();
     newSettingBill.updateCall(1)
-    assert.equal('1', newSettingBill.returnUpdateCall());
+    assert.equal(1, newSettingBill.returnUpdateCall());
   });
   it('should return sms cost', function(){
     var newSettingBill = SettingBill();
     newSettingBill.updateSms(1);
-    assert.equal('1', newSettingBill.returnUpdateSms());
+    assert.equal(1, newSettingBill.returnUpdateSms());
   });
 
   it('should return call total if call radio button is checked and update total', function(){
@@ -25,6 +25,16 @@ describe('Bill With Settings Widget Tests', function(){
     newBillSettings.billTypeCall('call');
     newBillSettings.billTypeSms('sms');
     assert.equal('', newBillSettings.settingsTotal());
+  });
+  it('should return updated warning level restrictions', function(){
+    var newBillSettings = SettingBill();
+    newBillSettings.updateWarning(20);
+    assert.equal(20, newBillSettings.updateWarning('20'));
+  });
+  it('should return updated critical level Restrictions', function(){
+    var newBillSettings = SettingBill();
+    newBillSettings.updateCritical(30);
+    assert.equal(30, newBillSettings.updateCritical('30'));
   });
   it('should return nothing if both radio buttons are not checked');
 });
